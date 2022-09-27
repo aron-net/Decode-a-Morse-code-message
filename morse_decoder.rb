6fecc4f3ee8ca1ecbf578940c9799fd1b6025d74
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-09-27 20:55:26 +0300

    decode word and sentence

--- morse_decoder.rb
+++ morse_decoder.rb
@@ -1,34 +1,53 @@
 @items = {
-    ".-" => "A", 
-    "-..." => "B", 
-    "-.-." => "C", 
-    "-.." => "D", 
-    "." => "E", 
-    "..-." => "F",
-    "--." => "G", 
-    "...." => "H", 
-    ".." => "I", 
-    ".---" => "J", 
-    "-.-" => "K", 
-    ".-.." => "L",
-    "--" => "M", 
-    "-." => "N", 
-    "---" => "O", 
-    ".--." => "P", 
-    "--.-" => "Q", 
-    ".-." => "R",
-    "..." => "S", 
-    "-" => "T", 
-    "..-" => "U", 
-    "...-" => "V", 
-    ".--" => "W", 
-    "-..-" => "X",
-    "-.--" => "Y", 
-    "--.." => "Z"
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
 }
 
 def get_letter(letter)
-    @character_letter = @items[letter]
-    @character_letter
+  @character_letter = @items[letter]
+  @character_letter
 end
 
+def decode_char(word)
+  @letters = word.split
+  @letters_word = ''
+  @letters.each do |letter|
+    get_letter(letter)
+    @letters_word += get_letter(letter)
+  end
+  print "#{@letters_word} "
+end
+
+def decode_word(sentence)
+  @message = sentence.split('   ')
+  @message.each do |word|
+    decode_char(word)
+  end
+end
+
+decode_word('-- -.--   -. .- -- .')
+decode_word('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
